2acea99f6c9e0796e3cbcc32963430db6cd1a8ba
khirthane <khirthane@gmail.com>
2021-10-23 13:41:55 +0200

    Add styles for Links

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,16 +1,17 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Logo from './logo';
 import t from 'utils/i18nProvider/translate';
 import './layout.scss'
 
 const Navbar = (): JSX.Element => {
+
     return (
         <aside className="side-bar">
             <Logo />
             <nav className="nav-container">
                 <div className="nav-content">
-                    <Link to="/" className="nav-link" id="dashboard-link">{t('dashboard')}</Link>
-                    <Link to="/employees" className="nav-link" id="employees-link">{t('employees')}</Link>
+                    <NavLink exact to="/" activeClassName='selected' className="nav-link" id="dashboard-link">{t('dashboard')}</NavLink>
+                    <NavLink to="/employees" activeClassName='selected' className="nav-link" id="employees-link">{t('employees')}</NavLink>
                 </div>
             </nav>
         </aside>
